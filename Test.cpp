38bd23470099cde29b11b3fbb464ba96a67531cc
Hila <hilashoshan0605@gmail.com>
2020-03-22 21:27:33 +0200

    start do my tests

--- Test.cpp
+++ Test.cpp
@@ -16,7 +16,7 @@ using namespace phonetic;
 #include <string>
 using namespace std;
 
-
+//Erel's tests: 
 
 TEST_CASE("Test replacement of p and b") {
     string text = "xxx happy yyy";
@@ -37,3 +37,50 @@ TEST_CASE("Test replacement of lower-case and upper-case") {
 }
 
 /* Add more test cases here */
+
+
+//Hila's tests
+
+TEST_CASE("Test v replaced by w case") {
+    string text = "Corona wirus please go from here FOREWER";
+    CHECK(find(text, "virus") == string("wiros")); 
+    CHECK(find(text, "VIRUS") == string("wiros")); 
+    CHECK(find(text, "forever") == string("FOREWER")); 
+    CHECK(find(text, "forewer") == string("FOREWER")); 
+}
+
+TEST_CASE("Test w replaced by v case") { 
+
+}
+
+TEST_CASE("Test return the currect word, v&w") {
+
+}
+
+TEST_CASE("Test b replaced by f case") {
+
+}
+
+TEST_CASE("Test b replaced by p case") {
+    
+}
+
+TEST_CASE("Test f replaced by b case") {
+    
+}
+
+TEST_CASE("Test f replaced by p case") {
+    
+}
+
+TEST_CASE("Test p replaced by f case") {
+    
+}
+
+TEST_CASE("Test p replaced by b case") {
+    
+}
+
+TEST_CASE("Test return the currect word, b&p&f") {
+
+}
