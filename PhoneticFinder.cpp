b0c7f85de3b69e253a31afb1f40de4ea207b08ec
Hila <hilashoshan0605@gmail.com>
2020-03-22 20:20:27 +0200

    add the neccecery files

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,12 @@
+#include "PhoneticFinder.hpp"
+
+using namespace std; 
+
+namespace phonetic {
+	string find(string text, string word) {
+		cout << "hi" << endl; 
+		return "";
+	}
+}
+
+
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+
+namespace phonetic{
+    	std::string find(std::string text, std::string word);
+}
+
